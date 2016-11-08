584534eae2222d0af38adbeda365b7e282528d0a
Rohan Büchner <rohan.buchner@manalto.com>
2016-11-08 23:16:35 +0200

    added config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 angular
-  .module('app', ['ui.router', 'chart.js']);
+  .module('app', ['ui.router', 'chart.js'])
+  .config(function (ChartJsProvider) {
+    ChartJsProvider
+      .setOptions({
+        colors: [
+          '#cf4646',
+          '#00ADF9',
+          '#DCDCDC',
+          '#46BFBD',
+          '#FDB45C',
+          '#949FB1',
+          '#4D5360']
+      });
+  });
