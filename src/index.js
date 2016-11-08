f76943d7997d82cac89738a5a40131997278c8c1
KyleNesium <the-kyle@hotmail.com>
2016-11-08 23:12:12 +0200

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 angular
-<<<<<<< HEAD
-  .module('app', ['ui.router','chart.js','mgcrea.ngStrap']);
-=======
-  .module('app', ['ui.router', 'chart.js']);
->>>>>>> 9746d8b443e6b9d6189aaa5a74207dc958bde182
+  .module('app', ['ui.router', 'chart.js', 'mgcrea.ngStrap']);
