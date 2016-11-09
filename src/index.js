40428b1bbc4bb23d22cbdf81ad1d69da00217daf
Rohan Büchner <rohan.buchner@manalto.com>
2016-11-09 16:38:26 +0200

    this is just a test commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 angular
   .module('app', ['ui.router', 'chart.js', 'mgcrea.ngStrap'])
   .config(function (ChartJsProvider) {
-    ChartJsProvider
-      .setOptions({
-        colors: [
-          '#cf4646',
-          '#00ADF9',
-          '#DCDCDC',
-          '#46BFBD',
-          '#FDB45C',
-          '#949FB1',
-          '#4D5360']
-      });
+    ChartJsProvider.setOptions({
+      colors: ['#97BBCD', '#DCDCDC', '#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360']
+    });
+    ChartJsProvider.setOptions('bubble', {
+      tooltips: {enabled: false}
+    });
   });
