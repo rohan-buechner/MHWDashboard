48d41e0fefbeab20630ef75e19a862f2893c2e60
Rohan Büchner <rohan.buchner@manalto.com>
2016-11-13 00:54:16 +0200

    added more subroutes and tried to tweak the gauges to no avail... fml

--- src/app/driving/driving.js
+++ src/app/driving/driving.js
@@ -3,6 +3,15 @@ angular
   .component('driving', {
     templateUrl: 'app/driving/driving.html',
     controller: DrivingController
+  })
+  .component('rear', {
+    template: '<img src="http://41.160.227.145:8083/mjpg/video.mjpg?COUNTER">'
+  })
+  .component('roof', {
+    template: '<img src="http://41.160.227.145:8083/mjpg/video.mjpg?COUNTER">'
+  })
+  .component('bottom', {
+    template: '<img src="http://41.160.227.145:8083/mjpg/video.mjpg?COUNTER">'
   });
 
 function DrivingController($log) {
--- src/routes.js
+++ src/routes.js
@@ -11,9 +11,9 @@ function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
     .state('app.info', {url: '/info', component: 'gauges'})
     .state('app.camp', {url: '/camp', component: 'camp'})
     .state('app.driving', {url: '/driving', component: 'driving'})
-    .state('app.driving.rear', {url: '/driving/rear', component: 'driving'})
-    .state('app.driving.roof', {url: '/driving/roof', component: 'driving'})
-    .state('app.driving.bottom', {url: '/driving/bottom', component: 'driving'})
+    .state('app.driving.rear', {url: '/rear', component: 'rear'})
+    .state('app.driving.roof', {url: '/roof', component: 'roof'})
+    .state('app.driving.bottom', {url: '/bottom', component: 'bottom'})
     .state('app.media', {url: '/media', component: 'media'})
     .state('app.topview', {url: '/topview', component: 'topview'});
 
