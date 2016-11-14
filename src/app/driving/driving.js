7cbb1037b2b1fbbf6ca97c7688c380b72379d8c9
Rohan Büchner <rohan.buchner@manalto.com>
2016-11-14 21:05:46 +0200

    added camera widths

--- src/app/driving/driving.js
+++ src/app/driving/driving.js
@@ -1,5 +1,5 @@
-var height = 500;
-var width = 500;
+var height = 100;
+var width = 100;
 
 angular
   .module('app')
@@ -8,13 +8,13 @@ angular
     controller: DrivingController
   })
   .component('rear', {
-    template: '<img src="http://41.160.227.145:8083/mjpg/video.mjpg?COUNTER" height="' + height + 'px" width="' + width + 'px" >'
+    template: '<img src="http://41.160.227.145:8083/mjpg/video.mjpg?COUNTER" height="' + height + '%" width="' + width + '%" >'
   })
   .component('roof', {
-    template: '<img src="http://85.93.105.195:8084/mjpg/video.mjpg?COUNTER" height="' + height + 'px" width="' + width + 'px" >'
+    template: '<img src="http://85.93.105.195:8084/mjpg/video.mjpg?COUNTER" height="' + height + '%" width="' + width + '%" >'
   })
   .component('bottom', {
-    template: '<img src="http://50.253.141.34:80/mjpg/video.mjpg?COUNTER" height="' + height + 'px" width="' + width + 'px" >'
+    template: '<img src="http://50.253.141.34:80/mjpg/video.mjpg?COUNTER" height="' + height + '%" width="' + width + '%" >'
   });
 
 function DrivingController($log) {
