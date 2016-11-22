b7fd9d18220c354dcf37e23dcc38bcdd3fb1e717
KyleNesium <the-kyle@hotmail.com>
2016-11-22 22:36:44 +0200

    added camp page buttons and linked commands(will hardcode the proper commands later)

--- src/app/camp/camp.js
+++ src/app/camp/camp.js
@@ -45,15 +45,97 @@ function CampController($log, WebIService, $interval, $scope) {
     WebIService.customCMD(cmd);
   };
 
-  // LHS Steady
+  // Left Steady
+  $scope.leftSteadyUp = function () {
+    var cmd = 'cmd=254,110,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.leftSteadyDown = function () {
+    var cmd = 'cmd=254,102,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.leftSteadyStop = function () {
+    var cmd = 'cmd=254,101,2r1t300';
+    WebIService.customCMD(cmd);
+  };
   // RHS Steady
-
+  $scope.rightSteadyUp = function () {
+    var cmd = 'cmd=254,110,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.rightSteadyDown = function () {
+    var cmd = 'cmd=254,102,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.rightSteadyStop = function () {
+    var cmd = 'cmd=254,101,2r1t300';
+    WebIService.customCMD(cmd);
+  };
   // Dish
+  $scope.dishUp = function () {
+    var cmd = 'cmd=254,110,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.dishDown = function () {
+    var cmd = 'cmd=254,102,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.dishStop = function () {
+    var cmd = 'cmd=254,101,2r1t300';
+    WebIService.customCMD(cmd);
+  };
   // TV (Inside)
-  // TV (Outside)
+  $scope.insideTvUp = function () {
+    var cmd = 'cmd=254,110,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.insideTvDown = function () {
+    var cmd = 'cmd=254,102,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.insideTvStop = function () {
+    var cmd = 'cmd=254,101,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+ // TV (Outside)
+  $scope.outsideTvUp = function () {
+    var cmd = 'cmd=254,110,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.outsideTvDown = function () {
+    var cmd = 'cmd=254,102,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.outsideTvStop = function () {
+    var cmd = 'cmd=254,101,2r1t300';
+    WebIService.customCMD(cmd);
+  };
   // Entry
+  $scope.entryStepUp = function () {
+    var cmd = 'cmd=254,110,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.entryStepDown = function () {
+    var cmd = 'cmd=254,102,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.entryStepStop = function () {
+    var cmd = 'cmd=254,101,2r1t300';
+    WebIService.customCMD(cmd);
+  };
   // Awning
-
+  $scope.awningUp = function () {
+    var cmd = 'cmd=254,110,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.awningDown = function () {
+    var cmd = 'cmd=254,102,2r1t300:cmd=254,109,2r1t300';
+    WebIService.customCMD(cmd);
+  };
+  $scope.awningStop = function () {
+    var cmd = 'cmd=254,101,2r1t300';
+    WebIService.customCMD(cmd);
+  };
   // Wutside light
   $scope.outsideLightOn = function () {
     var cmd = 'cmd=254,112,1r1t300';
