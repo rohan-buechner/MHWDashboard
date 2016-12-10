ae5e5f72aced9568e2cdcf7ecb9e2b1a3d92820e
Rohan Büchner <rohan.buchner@manalto.com>
2016-12-10 10:40:00 +0200

    button commands top view

--- src/app/topview/topview.js
+++ src/app/topview/topview.js
@@ -6,76 +6,77 @@ angular
   });
 
 function TopViewController($log, WebIService) {
-  $log.info('in top view controller');
+  var vm = this;
+  vm.compressorStatus = false;
 
-  this.compressorStatus = false;
-
-  WebIService.readRelays(1)
-    .then(function (data) {
-      this.compressorStatus = data.relays[1];
-    });
+  vm.$onInit = function () {
+    WebIService.readRelays(1)
+      .then(function (data) {
+        vm.compressorStatus = data.relays[1];
+      });
+  };
 
 // LHS front air suspension Pump
-  this.lhsFrontP = function () {
-    var cmd = 'cmd=254,110,1r1t300';
+  vm.lhsFrontP = function () {
+    var cmd = 'cmd=254,103,1r1t300:cmd=254,110,1r1t300';
     WebIService.customCMD(cmd);
   };
-  this.lhsFrontR = function () {
-    var cmd = 'cmd=254,111,1r1t300';
+  vm.lhsFrontR = function () {
+    var cmd = 'cmd=254,102,1r1t300:cmd=254,111,1r1t300';
     WebIService.customCMD(cmd);
   };
-  this.lhsFrontStop = function () {
+  vm.lhsFrontStop = function () {
     var cmd = 'cmd=254,102,1r1t300:cmd=254,103,1r1t300';
     WebIService.customCMD(cmd);
   };
 // RHS front air suspension Release
-  this.rhsFrontP = function () {
-    var cmd = 'cmd=254,112,1r1t300';
+  vm.rhsFrontP = function () {
+    var cmd = 'cmd=254,105,1r1t300:cmd=254,112,1r1t300';
     WebIService.customCMD(cmd);
   };
-  this.rhsFrontR = function () {
-    var cmd = 'cmd=254,113,1r1t300';
+  vm.rhsFrontR = function () {
+    var cmd = 'cmd=254,104,1r1t300:cmd=254,113,1r1t300';
     WebIService.customCMD(cmd);
   };
-  this.rhsFrontStop = function () {
+  vm.rhsFrontStop = function () {
     var cmd = 'cmd=254,104,1r1t300:cmd=254,105,1r1t300';
     WebIService.customCMD(cmd);
   };
   // LHS rear air suspension Pump
-  this.lhsRearP = function () {
+  vm.lhsRearP = function () {
     var cmd = 'cmd=254,114,1r1t300';
     WebIService.customCMD(cmd);
   };
-  this.lhsRearR = function () {
+  vm.lhsRearR = function () {
     var cmd = 'cmd=254,115,1r1t300';
     WebIService.customCMD(cmd);
   };
-  this.lhsRearStop = function () {
+  vm.lhsRearStop = function () {
     var cmd = 'cmd=254,106,1r1t300:cmd=254,107,1r1t300';
     WebIService.customCMD(cmd);
   };
   // RHS front air suspension Release
-  this.rhsRearP = function () {
+  vm.rhsRearP = function () {
     var cmd = 'cmd=254,108,2r1t300';
     WebIService.customCMD(cmd);
   };
-  this.rhsRearR = function () {
+  vm.rhsRearR = function () {
     var cmd = 'cmd=254,109,2r1t300';
     WebIService.customCMD(cmd);
   };
-  this.rhsRearStop = function () {
+  vm.rhsRearStop = function () {
     var cmd = 'cmd=254,100,2r1t300:cmd=254,101,2r1t300';
     WebIService.customCMD(cmd);
   };
   // Compressor for air suspension
-  this.compressorOn = function () {
+  vm.compressorOn = function () {
     var cmd = 'cmd=254,109,1r1t300';
-    this.compressorStatus = true;
+    vm.compressorStatus = true;
     toggleCompressor(cmd);
   };
-  this.compressorOff = function () {
+  vm.compressorOff = function () {
     var cmd = 'cmd=254,101,1r1t300';
-    this.compressorStatus = false;
+    vm.compressorStatus = false;
     toggleCompressor(cmd);
   };
   function toggleCompressor(cmd) {
@@ -83,7 +84,7 @@ function TopViewController($log, WebIService) {
       $log.debug('in success');
       WebIService.readRelays(1).then(function (data) {
         // position is the position of the item in the bank
-        this.compressorStatus = data.relays[1];
+        vm.compressorStatus = data.relays[1];
       });
     });
   }
